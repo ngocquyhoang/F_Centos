81b3cfa2957585e6fda832a9fa0530903a0f7f08
Hoàng Ngọc Quý <ichiwar@gmail.com>
2015-10-19 06:09:24 +0000

    Update code at 1:09PM

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :confirmable
   validates_uniqueness_of :user_name, :email
 end
