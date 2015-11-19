38bac28cde1b342183797b9686a8cff9a1726452
ngocquyhoang <ngocquyhoang.93@gmail.com>
2015-11-19 17:48:54 +0700

    Finish View

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,77 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+// Javascript
+$( document ).ready(function() {
+	// get and set width for content
+	// setting home page
+	// $('section.home').show(); 
+	// none scroll
+	$('html').css('overflow', 'hidden');
+	SetWidthValue();
+	$(".content-section").mCustomScrollbar({theme:"light-thin",scrollButtons:{enable:true}});
+	SetContentHeightValue();
+	ShowSocialView();
+	$(".footer .button-control .fa-arrow-circle-left").click( function () {
+		// SetViewbyBackButton();
+	});
+	$(".footer .button-control .fa-arrow-circle-right").click( function () {
+		// SetViewbyNextButton();
+	});
+	$("section.home .content-section .home-content button").click(function () {
+		SigninForm();
+	});
+	$(".signin-form #close").click(function () {
+		$("section.home .content-section .home-content .signin-form-box .signin-form").hide("slide", {direction:'up'} , "easeOutQuint ", 1000);
+		$("section.home .content-section .home-content .signin-form-box").hide("slide", {direction:'up'} , "easeOutQuint ", 1000);
+	})
+
+});
+// resize set value
+$( window ).resize(function() {
+	SetWidthValue();
+	SetContentHeightValue();
+});
+// function define
+function SetWidthValue () {
+	var width_vale = $(window).width();
+  $(".main-content").width(width_vale - 45);
+};
+// function set height content value
+function SetContentHeightValue () {
+	var height_value = $(window).height();
+	$("section .content-section").height(height_value - 110);
+};
+
+function ShowSocialView () {
+	$(".footer .social-connect ul li").each(function() {
+		$(this).click(function() {
+			var AttrName = $(this).attr('title');
+			$(".footer .social-view").toggle("slide", {direction:'right'} , "easeOutQuint ", 500);
+			switch (AttrName){
+			   case "Github": {SetSocialViewDetails("http://ngocquyhoang.github.io/", "assets/github.jpg", AttrName, "Connect us on <br />" + AttrName)};
+			   break;
+			   case "Facebook": {SetSocialViewDetails("https://www.facebook.com/ngocquyhoang.93", "assets/facebook.jpg", AttrName, "Connect us on <br />" + AttrName)};
+			   break;				   
+			   case "Goolge Plus": {SetSocialViewDetails("https://www.google.com", "assets/google-plus.jpg", AttrName, "Connect us on <br />" + AttrName)};
+			   break;
+			   default: {SetSocialViewDetails("https://twitter.com/malloryandbob", "assets/twitter.jpg", AttrName, "Connect us on <br />" + AttrName)};
+			};
+		});
+	});
+	$(".close-social-views-imgages").click(function() {
+		$(".footer .social-view").hide("slide", {direction:'right'} , "easeOutQuint ", 500);
+	});
+};
+function SetSocialViewDetails (link_href, image_src, title_des, button_des) {
+	$(".footer .social-view ul li figure figcaption a").attr("href", link_href);
+	$(".footer .social-view ul li figure div img").attr("src", image_src);
+	$(".footer .social-view h3").html(title_des);
+	$(".footer .social-view a").html(button_des);
+}
+function SigninForm () {
+	$("section.home .content-section .home-content .signin-form-box").show("slide", {direction:'up'} , "easeOutQuint ", 1000);
+	var width_vale = $(window).width();
+	$("section.home .content-section .home-content .signin-form-box .signin-form").css("margin-left",width_vale/2 - 355);
+	$("section.home .content-section .home-content .signin-form-box .signin-form").show("slide", {direction:'up'} , "easeOutQuint ", 1000);
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'home' => 'home#index'
+
+  get 'contact' => 'contact#index'
+
+  get 'about' => 'about#index'
+
+  get 'setting' => 'setting#index'
+
+  get 'blog' => 'blog#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
