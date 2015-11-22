c6c044c5151e29c2f71304fce15c337aea7fd59a
ngocquyhoang <quyhn@hiworld.com.vn>
2015-11-22 17:51:45 +0700

    Complete User update infomation

--- app/controllers/detail_controller.rb
+++ app/controllers/detail_controller.rb
@@ -1,7 +1,22 @@
 class DetailController < ApplicationController
-  def index
-  	if user_signed_in?
-  		@user_logs = current_user.user_logs
-  	end
-  end
+	def index
+		if user_signed_in?
+			@user_logs = current_user.user_logs
+		end
+	end
+	def update
+		@user_info = UserInfo.create(user_info_params)
+		user = current_user
+		user.name = @user_info.name
+		user.gender = @user_info.gender
+		user.age = @user_info.age
+		user.address = @user_info.address
+		user.p_num = @user_info.p_num
+		user.save
+		redirect_to :controller => 'detail', :action => 'index'
+	end
+
+	def user_info_params
+		params.require(:@user_info).permit(:name, :gender, :age, :address, :p_num)
+	end
 end
---
+++ db/migrate/20151122094629_create_user_infos.rb
@@ -0,0 +1,14 @@
+class CreateUserInfos < ActiveRecord::Migration
+  def change
+    create_table :user_infos do |t|
+      t.string :user_id, null: false, default: ""
+      t.string :name, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.string :gender, null: false, default: ""
+      t.integer :age, null: false, default: 0
+      t.string :p_num, null: false, default: ""
+
+      t.timestamps null: false
+    end
+  end
+end
